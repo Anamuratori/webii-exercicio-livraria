6a1e9c8b5d1dfebe46c4fcd7279ae04f3b0816f6
Muratori <arq.muratori@gmail.com>
2023-03-22 20:50:24 -0300

    mapper

--- src/main/java/br/com/ada/Livraria/model/mapper/LivroMapper.java
+++ src/main/java/br/com/ada/Livraria/model/mapper/LivroMapper.java
@@ -3,13 +3,29 @@ package br.com.ada.Livraria.model.mapper;
 import br.com.ada.Livraria.model.dto.LivroDTO;
 import br.com.ada.Livraria.model.entity.LivroEntity;
 
+import java.util.List;
+
 public class LivroMapper {
 
-    public LivroDTO livroDTO (LivroEntity livro) {
+    public LivroDTO update (LivroEntity livro) {
         LivroDTO livroDTO = new LivroDTO();
+        livroDTO.setId(livro.getId());
         livroDTO.setTitulo(livro.getTitulo());
         livroDTO.setGenero(livro.getGenero());
-
+        return livroDTO;
+    }
+    public LivroEntity update (LivroDTO livro) {
+        LivroEntity livroEntity = new LivroEntity();
+        livroEntity.setId(livro.getId());
+        livroEntity.setTitulo(livro.getTitulo());
+        livroEntity.setGenero(livro.getGenero());
+        return livroEntity;
+    }
+    public List<LivroDTO> listaLivroDTO (List<LivroEntity> listaLivroEntity) {
+        return listaLivroEntity.stream().map(categoriaEntity -> this.update(categoriaEntity)).toList();
+    }
+    public List<LivroEntity> listaLivroEntity (List<LivroDTO> listaLivroDTO) {
+        return listaLivroDTO.stream().map(categoriaDTO -> this.update(categoriaDTO)).toList();
     }
 
 }
